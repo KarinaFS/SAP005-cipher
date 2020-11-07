af5c9eec733bb73de5ee287ef0d615744c790d08
KarinaFS <karina_fsantos@outlook.com>
2020-11-06 22:22:48 -0300

    push

--- cipher.js
+++ cipher.js
@@ -1,5 +1,35 @@
 const cipher = {
-  // ...
+  let message = ""
+  const alphabetSize = 26;
+  const alphabetSizeASC = 65;
+  let displ = ;
+  let finalSentence = ""
+
+encode (displ, message) {
+
+for (let turns=0; turns < message.length; turns++ ){
+
+
+     let uppercase = message.toUpperCase()
+
+     let result = (message.charCodeAt(turns) - alphabetSizeASC + displ) % alphabetSize + alphabetSizeASC
+      
+     let print = String.fromCharCode(result)
+     finalSentence += print
+    }
+    return finalSentence;
+  } 
+
 };
+
+decode (displ, message){
+  
+let message = ""
+const alphabetSize = 26;
+const alphabetSizeASC = 65;
+let displ = ;
+let finalSentence = ""
+
+
 
 export default cipher;
--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+message.offset(displ);
+
+let finalSentence = 
+
+let btn_submit = document.getElementById ("btn_submit")
+btn_submit.addEventListener("click", finalSentence)
+
+function button (event) {
+
+event.preventDefault("btn_submit")
+
+
+
+console.log ("btn_submit")
+}
+
+function soma (a, b){
+
+ return a+b
+}
+
+soma ()
